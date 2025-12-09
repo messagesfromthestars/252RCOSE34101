ec5f408c120e169c762a0d541c6c82c61eb57b46
blownoutspeakers4 <corri137@umn.edu>
2025-12-09 20:00:12 +0900

    modify core.c

--- core.c
+++ core.c
@@ -4743,7 +4743,8 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	 * Make sure we do not leak PI boosting priority to the child.
 	 */
 	p->prio = current->normal_prio;
-
+	
+	/*user modified code*/
 	printk(KERN_INFO "a process forked\n");
 
 	uclamp_fork(p);
